2facb38bd15344534b5a0355dc30032948beda6c
adil_ma <itsadilhadid@gmail.com>
2023-08-30 01:27:48 +0100

    tasks 8 to 11

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,12 @@
-typedef int make_iso_compilers_happy;
+#include "binary_trees.h"
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (!tree)
+        return 0;
+
+    if (!(tree->parent))
+        return (0);
+
+    return (1 + binary_tree_depth(tree->parent));
+}
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,1 +1,12 @@
-typedef int make_iso_compilers_happy;
+#include "binary_trees.h"
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!tree || !func)
+        return;
+
+    binary_tree_postorder(tree->left, func);
+    binary_tree_postorder(tree->right, func);
+
+    func(tree->n);
+}
